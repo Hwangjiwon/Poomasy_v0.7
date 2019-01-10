3225ed07439aedca7643329d73b4ee9dd5d25fcc
jiwon <googims33@naver.com>
2019-01-10 11:28:39 +0900

    MemberDAO Implement

---
+++ Poomasy/src/main/java/com/bitproject/persistence/MemberDAOImpl.java
@@ -0,0 +1,39 @@
+package com.bitproject.persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.bitproject.domain.MemberVO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO{
+	@Inject
+	private SqlSession sqlSession;
+	private static final String namespace="com.bitproject.mappers.memberMapper";
+	public String getTime() {
+		return sqlSession.selectOne(namespace+"getTime");
+	}
+	@Override
+	public void insertMember(MemberVO vo) {
+		sqlSession.insert(namespace+".insertMember",vo);
+	}
+	@Override
+	public MemberVO selectMember(String userid) throws Exception{
+		return(MemberVO)
+				sqlSession.selectOne(namespace+".selectMember", userid);
+	}
+	@Override
+	public MemberVO readWithPW(String userid, String userpw) throws Exception{
+
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("userid", userid);
+		paramMap.put("userpw", userpw);
+
+		return sqlSession.selectOne(namespace+".readWithPW", paramMap);
+	}
+}
